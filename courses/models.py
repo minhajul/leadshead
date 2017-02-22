3077afe5c96f2a78007a48112de7139bafa0ebd3
minhajul <minhajul@maya.com.bd>
2017-02-22 21:57:37 +0600

    Added course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    location_choice = (
+        ('ACT - Canberra', 'ACT - Canberra'),
+        ('NSW - Parramatta', 'NSW - Parramatta'),
+        ('NSW - Sydney', 'NSW - Sydney'),
+        ('GLD - Brisbane', 'GLD - Brisbane'),
+        ('SA - Adelaide', 'SA - Adelaide'),
+        ('VIC - Melbourne', 'VIC - Melbourne'),
+        ('WA - Perth', 'WA - Perth'),
+        ('DH - Dhaka', 'DH - Dhaka'),
+    )
+    title = models.CharField(max_length=200)
+    fee = models.IntegerField()
+    location = models.CharField(max_length=100, choices=location_choice)
+    duration = models.CharField(max_length=50)
+    details = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.title
--- leadshead/settings.py
+++ leadshead/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'courses',
     'registration',
+    'courses',
 ]
 
 MIDDLEWARE = [
