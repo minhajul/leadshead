785e1ed152eb6b6d98ab05fc9e13bbb104082dad
minhajul <minhajul@maya.com.bd>
2017-02-22 22:52:04 +0600

    Added training model

--- leadshead/settings.py
+++ leadshead/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home',
     'registration',
     'courses',
+    'training'
 ]
 
 MIDDLEWARE = [
--- training/models.py
+++ training/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from registration.models import User
+from courses.models import Course
 
-# Create your models here.
+
+class Training(models.Model):
+    user_id = models.ForeignKey(User)
+    course_id = models.ForeignKey(Course)
+    participant = models.CharField(max_length=50)
+    date = models.DateField(default=None)
+    payment_method = models.CharField(max_length=30)
+    coupon = models.CharField(max_length=20)
+    is_send = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
