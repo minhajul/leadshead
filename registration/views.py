9ea70a36e4808c1fa4488818fca6aadf2d386acf
minhajul <minhajul@maya.com.bd>
2017-02-28 21:34:43 +0600

    Added login and register view

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register_view(request):
+    return render(request, 'register.html')
+
+
+def login_view(request):
+    return render(request, 'login.html')
