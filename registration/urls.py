292b7d1d155c51492f8b9aa047aa64e3db460763
minhajul <minhajul@maya.com.bd>
2017-03-03 00:56:52 +0600

    User registration updated

--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from registration.views import login_view
-from registration.views import register_view
+from registration.views import register_view, register_user
 
 urlpatterns = [
     url(r'^login/$', login_view, name='login'),
     url(r'^register/$', register_view, name='register'),
+    url(r'^register/user/$', register_user, name='register_user'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
 
 
 def register_view(request):
     return render(request, 'register.html')
+
+
+@csrf_protect
+def register_user(request):
+
+    User.objects.create_user(
+        username=request.POST['username'],
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        password=request.POST['password'],
+    )
+    return redirect('home.html')
 
 
 def login_view(request):
