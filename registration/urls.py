e375141b0103235cf1a46bd4346663ffb73fa28b
minhajul <minhajul@maya.com.bd>
2017-03-03 22:31:07 +0600

    Added user login system

--- registration/urls.py
+++ registration/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from registration.views import login_view
+from registration.views import login_view, login_user
 from registration.views import register_view, register_user
 
 urlpatterns = [
     url(r'^login/$', login_view, name='login'),
+    url(r'^login/user/$', login_user, name='login_user'),
     url(r'^register/$', register_view, name='register'),
     url(r'^register/user/$', register_user, name='register_user'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
+def login_view(request):
+    return render(request, 'login.html')
+
+
+@csrf_protect
+def login_user(request):
+    if request.method == "POST":
+        # email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.error(request, "Something went wrong")
+                return redirect('login')
+        else:
+            messages.error(request, "Authentication Failed!")
+            return redirect('login')
+    else:
+        return redirect('login')
 
 
 def register_view(request):
@@ -9,7 +36,6 @@ def register_view(request):
 
 @csrf_protect
 def register_user(request):
-
     User.objects.create_user(
         username=request.POST['username'],
         first_name=request.POST['first_name'],
@@ -17,8 +43,5 @@ def register_user(request):
         email=request.POST['email'],
         password=request.POST['password'],
     )
+    messages.success(request, 'Registration Success')
     return redirect('home.html')
-
-
-def login_view(request):
-    return render(request, 'login.html')
