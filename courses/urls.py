11c77dbcab66d51699fd25ce423cace79aa5c9d0
minhajul <minhajul@maya.com.bd>
2017-03-05 00:11:15 +0600

    Added generic view and pagination

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from courses.views import courses
+from courses.views import CourseListView, CourseDetailsView
 
 urlpatterns = [
-    url(r'^$', courses, name='courses')
+    url(r'^$', CourseListView.as_view(), name='courses'),
+    url(r'^details/(?P<pk>[0-9]+)/$', CourseDetailsView.as_view(), name='details'),
+
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import generic
 from courses.models import Course
 
 
-def courses(request):
-    all_courses = Course.objects.all().order_by('-id')
-    return render(request, 'courses.html', {'courses': all_courses})
+class CourseListView(generic.ListView):
+    model = Course
+    template_name = 'courses.html'
+    context_object_name = 'courses'
+    paginate_by = 2
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page', self.paginate_by)
+
+
+class CourseDetailsView(generic.DetailView):
+    model = Course
+    template_name = 'details.html'
+    context_object_name = 'course'
+
+# def courses(request):
+#     all_courses = Course.objects.all().order_by('-id')
+#     return render(request, 'courses.html', {'courses': all_courses})
