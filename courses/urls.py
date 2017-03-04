94b2e137bfbd43021d4fb7110d1bb5f73bb0405b
minhajul <minhajul@maya.com.bd>
2017-03-05 00:13:40 +0600

    Updated

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,4 @@ from courses.views import CourseListView, CourseDetailsView
 urlpatterns = [
     url(r'^$', CourseListView.as_view(), name='courses'),
     url(r'^details/(?P<pk>[0-9]+)/$', CourseDetailsView.as_view(), name='details'),
-
 ]
